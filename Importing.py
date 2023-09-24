2f4978d47ec36c8c4a34e197dc0c21c68029dbe6
Akanji Ayomide Gideon <110993463+Ayomidegideon123@users.noreply.github.com>
2023-09-24 05:26:05 +0100

    Create importing.py

---
+++ Importing.py
@@ -0,0 +1,13 @@
+# importing the required modules
+from tkinter import*
+# importing all the modules and classes from tkinter
+fromtkinterimport ttk as ttk
+# importing the ttk module from tkinter
+from tkinter import message box as mb
+# importing the messagebox module from tkinter
+import datetime 
+# importing the datetime module
+import sqlite3
+# importing the sqlite3 module
+from tkcalendar import DateEntry
+# importing the DateEntry class from the tkcalendar module
